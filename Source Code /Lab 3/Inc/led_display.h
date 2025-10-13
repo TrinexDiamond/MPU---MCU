51b2cf7b1c156bb1edfb97c40fab696cfd357c13
Trịnh Viết Thụ <thu.trinh2353157@hcmut.edu.vn>
2025-10-13 22:07:54 +0700

    Add files via upload

---
+++ Source Code /Lab 3/Inc/led_display.h
@@ -0,0 +1,13 @@
+/*
+ * led_display.h
+ *
+ *  Created on: 10 thg 10, 2025
+ *      Author: Trinh Viet Thu
+ */
+
+#ifndef INC_LED_DISPLAY_H_
+#define INC_LED_DISPLAY_H_
+
+void led_mode_display(int index);
+
+#endif /* INC_LED_DISPLAY_H_ */
