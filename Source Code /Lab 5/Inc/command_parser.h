aab28d33b1355a57ca561a955575151af1144648
Trịnh Viết Thụ <thu.trinh2353157@hcmut.edu.vn>
2025-11-12 18:10:11 +0700

    Add files via upload

---
+++ Source Code /Lab 5/Inc/command_parser.h
@@ -0,0 +1,21 @@
+/*
+ * command_parser.h
+ *
+ *  Created on: 8 thg 11, 2025
+ *      Author: Trinh Viet Thu
+ */
+
+#ifndef INC_COMMAND_PARSER_H_
+#define INC_COMMAND_PARSER_H_
+
+#include "main.h"
+
+#define CMD_NO_CMD  0
+#define CMD_RST     1
+#define CMD_OK      2
+
+void command_parser_fsm(uint8_t received_char);
+
+uint8_t get_command_flag(void);
+
+#endif /* INC_COMMAND_PARSER_H_ */
---
+++ Source Code /Lab 5/Inc/uart_comm.h
@@ -0,0 +1,15 @@
+/*
+ * uart_comm.h
+ *
+ *  Created on: 8 thg 11, 2025
+ *      Author: Trinh Viet Thu
+ */
+
+#ifndef INC_UART_COMM_H_
+#define INC_UART_COMM_H_
+
+#include "main.h"
+
+void uart_communication_fsm(void);
+
+#endif /* INC_UART_COMM_H_ */
